682f8e3c639819042edf84bae332e21d622cc6bc
elnatalya88 <elnatalya888@gmail.com>
2024-10-05 22:59:10 +1000

    Add files via upload

---
+++ peremennye.py
@@ -0,0 +1,5 @@
+kolichestvo_vypolnennyh_DZ = 12
+kolichestvo_zatrachennyh_chasov = 1.5
+nameKurs = 'Python'
+sr_vremya_vypoln = 0.125
+print("Курс:", nameKurs,'всего задач:',kolichestvo_vypolnennyh_DZ,'затрачено часов:', kolichestvo_zatrachennyh_chasov,',' 'среднее время выполнения', sr_vremya_vypoln,',''часа')
